7fcf77fcab75de0a57f7e9b538d9e3eac07ebfa2
Ezzalddeen Ali <Ezzo0@outlook.sa>
2020-08-08 03:32:58 +0300

    Update environment.pre.ts

--- src/environments/environment.pre.ts
+++ src/environments/environment.pre.ts
@@ -12,17 +12,19 @@ export const environment = {
   remoteConfig: false,
   remoteConfigUrl: '/widget-config.json',
   loadRemoteTranslations: true,
-  remoteTranslationsUrl: 'https://tiledesk-server-pre.herokuapp.com/',
+  remoteTranslationsUrl: 'https://ersal-chat.herokuapp.com/',
 
   firebase: {
-    apiKey: 'CHANGEIT',
-    authDomain: 'CHANGEIT',
-    databaseURL: 'CHANGEIT',
-    projectId: 'CHANGEIT',
-    storageBucket: 'CHANGEIT',
-    messagingSenderId: 'CHANGEIT'
+      apiKey: "AIzaSyDqdtylO2v88ZE6-GxGlL_pe_H6eww7IL4",
+    authDomain: "yemen-food.firebaseapp.com",
+    databaseURL: "https://yemen-food.firebaseio.com",
+    projectId: "yemen-food",
+    storageBucket: "yemen-food.appspot.com",
+    messagingSenderId: "907903462252",
+    appId: "1:907903462252:web:e00d381df1f9771ba32149",
+    measurementId: "G-JZ4NQ4B2DB"
   },
-  apiUrl: 'https://tiledesk-server-pre.herokuapp.com/',
+  apiUrl: 'https://ersal-chat.herokuapp.com/',
   tenant: 'tilechat',
   defaultLang : 'en',
   shemaVersion : '4'
